9de84a7f636e43a18db4f5cc72cf27cf9942d132
sonu25122000 <skg74827@gmail.com>
2023-02-27 20:30:52 +0530

    resume link correction

--- src/Components/TopNavbar.jsx
+++ src/Components/TopNavbar.jsx
@@ -1,19 +1,26 @@
 import "../Style/topNavbar.css";
 import React, { useState } from "react";
 import logo from "../Components/Images/logo.jpeg";
+import resume from "../Components/Images/Sonu_Kumar_Resume.pdf";
 import NavDrawer from "./NavDrawer";
-
 
 const TopNavbar = () => {
   const [current, setCurrent] = useState("#home");
   return (
     <div>
-      <div style={{height:"10vh"}} id="topnav">
-        <a  href="https://sonu25122000.github.io/"><div  style={{ marginLeft: "40px", align: "center", padding: "4px" }}>
-          <img width={"70px"} src={logo} alt="logo" />
-        </div></a>
+      <div style={{ height: "10vh" }} id="topnav">
+        <a href="https://sonu25122000.github.io/">
+          <div style={{ marginLeft: "40px", align: "center", padding: "4px" }}>
+            <img width={"70px"} src={logo} alt="logo" />
+          </div>
+        </a>
         <div id="navel">
-          <div onClick={() => [setCurrent("#home"),window.location.href="#home"]}>
+          <div
+            onClick={() => [
+              setCurrent("#home"),
+              (window.location.href = "#home"),
+            ]}
+          >
             {" "}
             <a href="#home">
               <h4 className={current === "#home" ? "activemain" : null}>
@@ -21,28 +28,48 @@ const TopNavbar = () => {
               </h4>
             </a>{" "}
           </div>
-          <div onClick={() => [setCurrent("#about"),window.location.href="#about"]}>
+          <div
+            onClick={() => [
+              setCurrent("#about"),
+              (window.location.href = "#about"),
+            ]}
+          >
             <a href="#about">
               <h4 className={current === "#about" ? "activemain" : null}>
                 About
               </h4>
             </a>{" "}
           </div>
-          <div onClick={() => [setCurrent("#skills"),window.location.href="#skills"]}>
+          <div
+            onClick={() => [
+              setCurrent("#skills"),
+              (window.location.href = "#skills"),
+            ]}
+          >
             <a href="#skills">
               <h4 className={current === "#skills" ? "activemain" : null}>
                 Skills
               </h4>
             </a>{" "}
           </div>
-          <div onClick={() => [setCurrent("#projects"),window.location.href="#projects"]}>
+          <div
+            onClick={() => [
+              setCurrent("#projects"),
+              (window.location.href = "#projects"),
+            ]}
+          >
             <a href="#projects">
               <h4 className={current === "#projects" ? "activemain" : null}>
                 Projects
               </h4>
             </a>{" "}
           </div>
-          <div onClick={() => [setCurrent("#contact"),window.location.href="#contact"]}>
+          <div
+            onClick={() => [
+              setCurrent("#contact"),
+              (window.location.href = "#contact"),
+            ]}
+          >
             <a href="#contact">
               <h4 className={current === "#contact" ? "activemain" : null}>
                 Contact
@@ -50,17 +77,20 @@ const TopNavbar = () => {
             </a>{" "}
           </div>
           <div onClick={() => setCurrent("#resume")}>
-            <a  href="https://onedrive.live.com/?authkey=%21AAdLQK8VH%5FysUEw&cid=8C56F2E1EF84D28C&id=8C56F2E1EF84D28C%21106&parId=root&o=OneUp" target="_blank" rel="noreferrer" download={true}>
-              <h4 id="resumenav" className={current === "#resume" ? "activemain" : null}>
+            {/* <a  href="https://onedrive.live.com/?authkey=%21AAdLQK8VH%5FysUEw&cid=8C56F2E1EF84D28C&id=8C56F2E1EF84D28C%21106&parId=root&o=OneUp" target="_blank" rel="noreferrer" download={true}> */}
+            <a href={resume} target="_blank" rel="noreferrer" download>
+              <h4
+                id="resumenav"
+                className={current === "#resume" ? "activemain" : null}
+              >
                 Resume
-              </h4>                                                                       
+              </h4>
             </a>{" "}
           </div>
-         
         </div>
         <div id="sidebar">
-          <NavDrawer/>
-          </div>
+          <NavDrawer />
+        </div>
       </div>
     </div>
   );
